2bcf63a6bad3f0bb73142a2dc3e6627abee28112
Josh <joshua.l.mertens@gmail.com>
2024-03-05 18:32:41 +1100

    feat: add mediaItems types and fetch with search criteria

--- src/services/images/images.ts
+++ src/services/images/images.ts
@@ -1,27 +1,56 @@
-import { Images } from "./types.ts";
+import ky from "ky";
+import { Images, MediaItemSearch } from "./types.ts";
+
+const currentDate = new Date();
+const searchParams: MediaItemSearch = {
+  pageSize: 50,
+  filters: {
+    mediaTypeFilter: {
+      mediaTypes: "PHOTO",
+    },
+    includeArchivedMedia: true,
+    dateFilter: {
+      dates: [
+        {
+          year: 0,
+          day: currentDate.getDate(),
+          month: currentDate.getMonth() + 1, // month starts from 0
+        },
+      ],
+    },
+  },
+};
 
 export const fetchPhotos = async (access_token: string) => {
   try {
-    const params = new URLSearchParams();
-    params.append("pageSize", "3");
-    const res = await fetch(
-      "https://photoslibrary.googleapis.com/v1/mediaItems",
-      {
-        method: "get",
-        headers: {
-          Authorization: `Bearer ${access_token}`,
-          "Content-Type": "application/json",
-        },
-      }
-    );
+    const client = ky.create({
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+      json: searchParams,
+    });
+    const endpoint =
+      "https://photoslibrary.googleapis.com/v1/mediaItems:search";
+
+    const res = await client.post(endpoint);
+
     const images: Images = await res.json();
-    const urls = images.mediaItems.map(({ baseUrl, id }) => ({
-      source: baseUrl,
-      id,
-    }));
+
+    if (!images.mediaItems?.length) {
+      return [];
+    }
+
+    const urls = images.mediaItems.map(
+      ({ baseUrl, id, mediaMetadata: { height, width } }) => ({
+        source: baseUrl,
+        id,
+        height,
+        width,
+      })
+    );
+
     return urls;
   } catch (err) {
-    console.log("ERROR");
-    console.error(err);
+    console.error("ERROR", err);
   }
 };
--- src/services/images/types.ts
+++ src/services/images/types.ts
@@ -1,3 +1,4 @@
+// https://developers.google.com/photos/library/reference/rest/v1/mediaItems
 interface CameraDetails {
   cameraMake: string;
   cameraModel: string;
@@ -5,14 +6,14 @@ interface CameraDetails {
 
 interface Video extends CameraDetails {
   fps: number;
-  status: string;
+  status: "UNSPECIFIED" | "PROCESSING" | "READY" | "FAILED";
 }
 
 interface Photo extends CameraDetails {
   focalLength: number;
   apertureFNumber: number;
   isoEquivalent: number;
-  exposureTime: string;
+  exposureTime: string; //time ending in s
 }
 
 interface MediaItems {
@@ -24,10 +25,87 @@ interface MediaItems {
     | { video: Video; photo: never }
     | { photo: Photo; video: never }
   ) & {
-    creationTime: Date;
+    creationTime: string; // Timestamp
     width: string;
     height: string;
   };
 }
 
 export type Images = { mediaItems: MediaItems[] };
+
+/************************
+ SEARCH OPTIONS
+ ************************/
+// https://developers.google.com/photos/library/reference/rest/v1/mediaItems/search
+interface Date {
+  // include all but set unimportant props to 0
+  year: number;
+  month: number;
+  day: number;
+}
+type DateFilter = Partial<{
+  // Max 5 Dates and/or Ranges
+  dates: Date[];
+  ranges: {
+    startDate: Date;
+    endDate: Date;
+  }[];
+}>;
+
+type ContentCategories =
+  | "NONE"
+  | "LANDSCAPE"
+  | "RECEIPTS"
+  | "CITYSCAPES"
+  | "LANDMARKS"
+  | "SELFIES"
+  | "PEOPLE"
+  | "PETS"
+  | "WEDDINGS"
+  | "BIRTHDAYS"
+  | "DOCUMENTS"
+  | "TRAVEL"
+  | "ANIMALS"
+  | "FOOD"
+  | "SPORT"
+  | "NIGHT"
+  | "PERFORMANCES"
+  | "WHITEBOARDS"
+  | "SCREENSHOTS"
+  | "UTILITY"
+  | "ARTS"
+  | "CRAFTS"
+  | "FASHION"
+  | "HOUSES"
+  | "GARDENS"
+  | "FLOWERS"
+  | "HOLIDAYS";
+
+type ContentFilter = Partial<{
+  includedContentCategories: ContentCategories[];
+  excludedContentCategories: ContentCategories[];
+}>;
+
+type MediaTypes = "ALL_MEDIA" | "PHOTO" | "VIDEO";
+interface MediaTypeFilter {
+  mediaTypes: MediaTypes;
+}
+interface FeatureFilter {
+  includedFeatures: "NONE" | "FAVORITES";
+}
+type Filters = Partial<{
+  dateFilter: DateFilter;
+  contentFilter: ContentFilter;
+  mediaTypeFilter: MediaTypeFilter;
+  featureFilter: FeatureFilter;
+  includeArchivedMedia: boolean;
+  excludeNonAppCreatedData: boolean;
+}>;
+
+export type MediaItemSearch = Partial<{
+  albumId: string;
+  pageSize: number;
+  pageToken: string;
+  filters: Filters;
+  orderBy: string;
+}>;
