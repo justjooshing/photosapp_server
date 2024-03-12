07adfe44278589b6786887776a6b42e8008a8cb2
Josh <joshua.l.mertens@gmail.com>
2024-03-12 16:39:47 +1100

    return images from DB matching the month/day of current_date

--- src/services/images/images.ts
+++ src/services/images/images.ts
@@ -2,6 +2,45 @@ import ky from "ky";
 import { Images, MediaItemSearch } from "./types.ts";
 import { prisma } from "../../loaders/prisma.ts";
 import { getUserData } from "../user/user.ts";
+import { Prisma, Images as SchemaImages } from "@prisma/client";
+
+const identifyNewImages = async (
+  userId: number,
+  images: Images["mediaItems"] = []
+) => {
+  const newImages: Images["mediaItems"] = [];
+  for (const image of images) {
+    const existingImage = await prisma.images.findUnique({
+      where: {
+        userId,
+        googleId: image.id,
+      },
+    });
+    if (!existingImage) {
+      newImages.push(image);
+    }
+  }
+  return newImages;
+};
+
+const updateImagesDB = async (
+  userId: number,
+  images: Images["mediaItems"] = []
+) => {
+  const newImages = await identifyNewImages(userId, images);
+  if (!!newImages.length) {
+    await prisma.images.createMany({
+      data: newImages.map((image) => ({
+        googleId: image.id,
+        userId,
+        photoUrl: image.baseUrl,
+        created_at: image.mediaMetadata.creationTime,
+        width: Number(image.mediaMetadata.width),
+        height: Number(image.mediaMetadata.height),
+      })),
+    });
+  }
+};
 
 const currentDate = new Date();
 const searchParams: MediaItemSearch = {
@@ -23,39 +62,6 @@ const searchParams: MediaItemSearch = {
   },
 };
 
-const identifyNewImages = async (userId: number, images: Images) => {
-  const newImages: Images["mediaItems"] = [];
-
-  for (const image of images.mediaItems) {
-    const existingImage = await prisma.images.findUnique({
-      where: {
-        userId,
-        googleId: image.id,
-      },
-    });
-    if (!existingImage) {
-      newImages.push(image);
-    }
-  }
-  return newImages;
-};
-
-const updateImagesDB = async (userId: number, images: Images) => {
-  const newImages = await identifyNewImages(userId, images);
-  if (!!newImages.length) {
-    await prisma.images.createMany({
-      data: newImages.map((image) => ({
-        googleId: image.id,
-        userId,
-        photoUrl: image.baseUrl,
-        created_at: image.mediaMetadata.creationTime,
-        width: Number(image.mediaMetadata.width),
-        height: Number(image.mediaMetadata.height),
-      })),
-    });
-  }
-};
-
 export const getImages = async (access_token: string) => {
   try {
     const client = ky.create({
@@ -71,16 +77,14 @@ export const getImages = async (access_token: string) => {
     const newImages = await res.json<Images>();
 
     const { id: userId } = await getUserData(access_token);
-    await updateImagesDB(userId, newImages);
+    await updateImagesDB(userId, newImages.mediaItems);
 
-    const images = await prisma.images.findMany({
-      where: {
-        userId,
-        created_at: {
-          //what do I want to put here
-        },
-      },
-    });
+    const sql = Prisma.sql`
+    SELECT * FROM "Images" WHERE "userId" = ${userId}
+    AND EXTRACT(MONTH FROM "created_at") = EXTRACT(MONTH FROM CURRENT_DATE)
+    AND EXTRACT(YEAR FROM "created_at") = EXTRACT(MONTH FROM CURRENT_DATE);`;
+
+    const images = await prisma.$queryRaw<SchemaImages[]>(sql);
 
     if (!images.length) {
       return [];
--- src/services/user/user.ts
+++ src/services/user/user.ts
@@ -3,6 +3,25 @@ import { UserData } from "./types.ts";
 import { prisma } from "../../loaders/prisma.ts";
 import { User } from "@prisma/client";
 
+const updateUsersDB = async ({ email, id, picture }: UserData) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+  if (!user) {
+    const newUser = await prisma.user.create({
+      data: {
+        googleId: id,
+        email,
+        googleProfilePicture: picture,
+      },
+    });
+    return newUser;
+  }
+  return user;
+};
+
 export const getUserData = async (access_token: string): Promise<User> => {
   try {
     const client = ky.create({
@@ -12,23 +31,8 @@ export const getUserData = async (access_token: string): Promise<User> => {
     });
     const endpoint = "https://www.googleapis.com/oauth2/v1/userinfo";
     const res = await client.get(endpoint);
-    const { id, email, picture } = await res.json<UserData>();
-    const user = await prisma.user.findUnique({
-      where: {
-        email,
-      },
-    });
-    if (!user) {
-      const newUser = await prisma.user.create({
-        data: {
-          googleId: id,
-          email,
-          googleProfilePicture: picture,
-        },
-      });
-      return newUser;
-    }
-    return user;
+    const user = await res.json<UserData>();
+    return await updateUsersDB(user);
   } catch (err) {
     console.error("ERROR", err);
     throw err;
