ff81f4493aa56880a8298bbabaf215f89ce97a0d
Josh <joshua.l.mertens@gmail.com>
2024-03-25 20:35:42 +1100

    refactor: move jwt checking to all /api routes

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import { albums, count, images, login } from "./routes/index.ts";
+import { checkJWT } from "./middlewares/auth/index.ts";
+import { getUserData } from "./middlewares/user/index.ts";
 
 export const routes = () => {
   const app = Router();
+  app.use(checkJWT, getUserData);
+
   images(app);
   login(app);
   albums(app);
--- src/api/routes/images.ts
+++ src/api/routes/images.ts
@@ -1,13 +1,10 @@
 import { Router } from "express";
-import { getUserData } from "../middlewares/user/index.ts";
-import { checkJWT } from "../middlewares/auth/index.ts";
 import { ImagesController } from "../controllers/images.ts";
 
 const route = Router();
 
 export const images = (app: Router) => {
   app.use("/images", route);
-  route.use(checkJWT, getUserData);
   route.get("/", ImagesController.getImagesByType);
   route.post("/", ImagesController.handleSortOrDeletePhotos);
 };
