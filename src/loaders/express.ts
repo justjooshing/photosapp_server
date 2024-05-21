6139a2e837d330a5c643f625373d8cf4fd704c1a
Josh <joshua.l.mertens@gmail.com>
2024-05-21 10:29:46 +1000

    chore: remove cookies-parser as no longer using them

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,7 +1,6 @@
 import bodyParser from "body-parser";
 import cors from "cors";
 import express, { Express } from "express";
-import cookies from "cookie-parser";
 import { routes } from "../api/index.js";
 import { CONFIG } from "../config/index.js";
 import { MiddlewareProps } from "../api/services/images/types.js";
@@ -10,7 +9,6 @@ import { handleCorsOrigin } from "@/utils/index.js";
 export const expressSetup = (app: Express) => {
   app.use(cors({ origin: handleCorsOrigin }));
   app.use(express.json());
-  app.use(cookies());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use((req, _, next) => {
     if (!req?.locals) {
