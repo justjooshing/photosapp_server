3ff84a9db349830deb44688ea8e694f767078ea6
Josh <joshua.l.mertens@gmail.com>
2024-08-06 10:06:56 +1000

    chore: remove unnecessary scope

--- src/config/index.ts
+++ src/config/index.ts
@@ -29,10 +29,8 @@ export const CONFIG = {
     auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
     client_secret: process.env.GOOGLE_CLIENT_SECRET as string,
     redirect_uris: [`${baseURL}/api/auth/google/callback`],
-    // https://developers.google.com/photos/library/reference/rest/v1/mediaItems
     scopes: [
       "https://www.googleapis.com/auth/photoslibrary.readonly",
-      "https://www.googleapis.com/auth/photoslibrary.readonly.originals",
       "https://www.googleapis.com/auth/userinfo.email",
     ],
   },
